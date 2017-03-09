e2245a45097d4c45105aa21febbfca5c1a343874
Connor <connordear@gmail.com>
2017-03-09 09:45:30 -0500

    updated goHome script

--- python/goHome.py
+++ python/goHome.py
@@ -1,8 +1,14 @@
 # This function will send the feeder block back to it's home position
 
 
-import Slush 
+import Slush
+import time
 
-def goHome(motor):	
-	motor.goTo(0)
-	
+board = Slush.sBoard()
+lat_motor = Slush.Motor(0)
+lat_motor.run(0,500)
+time.sleep(2)
+lat_motor.run(1,500)
+time.sleep(2)
+lat_motor.run(1,0)
+
--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ router.get('/home', function(req, res, next) {
   console.log("routing worked");
   // creat child_process
     const spawn = require('child_process').spawn;
-    const process = spawn('python',["./python/test.py"]);
+    const process = spawn('python',["./python/goHome.py"]);
     process.stdout.on('data', function(data){
       res.status(200).send(data);
     });
