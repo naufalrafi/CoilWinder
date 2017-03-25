154e07f31d0fdf0fa99f6ee312e7ce407e6d2120
Connor <connordear@gmail.com>
2017-03-24 20:28:44 -0400

    Updated homing scripts

--- python/setHome.py
+++ python/setHome.py
@@ -4,9 +4,9 @@ import sys
 board = Slush.sBoard()
 lateral_motor = Slush.Motor(0)
 
-lateral_motor.goUntilPress(1,0,1000)
+lateral_motor.goUntilPress(1,1,100000)
 lateral_motor.setAsHome()
-lateral_motor.goUntilPress(1,1,1000)
+lateral_motor.goUntilPress(1,0,100000)
 end = lateral_motor.getPosition()
 lateral_motor.goTo(0)
 
